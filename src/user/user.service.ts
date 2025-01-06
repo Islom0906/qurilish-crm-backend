79c39fda21279a495241c3d6881be4661f106c35
islom <iabdugofurov99@gmail.com>
2025-01-06 14:01:38 +0500

    Bismillah

---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {User, UserDocument} from "./user.model";
+import {Model} from "mongoose";
+
+@Injectable()
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel:Model<UserDocument>) {}
+
+    async byId(id:string){
+        const user=await  this.userModel.findById(id)
+
+        if (!user) throw new NotFoundException('Not Found User')
+
+        return user
+    }
+
+}
+
