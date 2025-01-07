67d90459e08a1f9829450ed47626ddeeb7719c6c
islom <iabdugofurov99@gmail.com>
2025-01-07 13:19:38 +0500

    mongo uri env orqali ulandi

--- dist/config/mongo.config.js
+++ dist/config/mongo.config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getMongoDbConfig = void 0;
 const getMongoDbConfig = async (configService) => {
     return {
-        uri: configService.get('mongodb+srv://abdugofurovislom1:sSv5vf0rxcENiSZx@qurilish.hieyx.mongodb.net/?retryWrites=true&w=majority&appName=Qurilish')
+        uri: configService.get('MONGODB_URI')
     };
 };
 exports.getMongoDbConfig = getMongoDbConfig;
