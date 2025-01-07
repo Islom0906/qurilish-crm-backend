94857c5a5ed0ab68b210b0bbbdda2ed550f19ba8
islom <iabdugofurov99@gmail.com>
2025-01-07 13:31:46 +0500

    dist papka ochib

--- dist/auth/dto/registerDto.d.ts
+++
@@ -1,7 +0,0 @@
-import { RoleUser } from "../../user/user.interface";
-export declare class RegisterDto {
-    fullName: string;
-    email: string;
-    password: string;
-    role: RoleUser;
-}
--- dist/config/mongo.config.d.ts
+++
@@ -1,3 +0,0 @@
-import { ConfigService } from "@nestjs/config";
-import { MongooseModuleOptions } from "@nestjs/mongoose";
-export declare const getMongoDbConfig: (configService: ConfigService) => Promise<MongooseModuleOptions>;
