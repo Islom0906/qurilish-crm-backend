11543dded2de20edadf933aa65ee7f84f9707434
islom <iabdugofurov99@gmail.com>
2025-01-07 16:22:11 +0500

    port 4100

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
       .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 4100);
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 bootstrap();
