45ccfa7e2ea9b2457f671bccd75198446364f91d
islom <iabdugofurov99@gmail.com>
2025-01-07 16:32:22 +0500

    dist ochirildi

--- dist/auth/decorators/auth.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-import { RoleUser } from "../../user/user.interface";
-export declare const Auth: (role?: RoleUser) => <TFunction extends Function, Y>(target: TFunction | object, propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<Y>) => void;
